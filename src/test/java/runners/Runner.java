fe8f2b311b9f5d1e23fff505998d72d19d3c38a3
MONSTER <erelozturk2@gmail.com>
2023-06-21 06:47:22 +0300

    v1

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,18 @@
+package runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        tags = "@US01Api",
+        glue = {"stepdefinitions","hooks"},
+        features = "./src/test/resources/",
+        dryRun = false
+)
+
+public class Runner {
+
+
+}
